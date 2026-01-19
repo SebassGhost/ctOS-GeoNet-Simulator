7079ab6b38049a800b163da2661e64df49038035
Sebastián Quintero <sq07369@gmail.com>
2026-01-18 20:24:50 -0500

    Update Node.js

--- src/entities/Node.js
+++ src/entities/Node.js
@@ -1,1 +1,23 @@
+export default class Node {
+  constructor({ id, lat, lng, type = "device", status = "normal" }) {
+    this.id = id;
+    this.lat = lat;
+    this.lng = lng;
+    this.type = type;
+    this.status = status;
+  }
 
+  draw(ctx, map) {
+    const pos = map.latLngToContainerPoint([this.lat, this.lng]);
+
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, this.type === "ctos" ? 12 : 5, 0, Math.PI * 2);
+
+    ctx.fillStyle =
+      this.status === "normal" ? "#00ffcc" :
+      this.status === "unstable" ? "#ffaa00" :
+      "#ff3355";
+
+    ctx.fill();
+  }
+}
