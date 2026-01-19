c06af9238ed60b7f690599e99e402d77d9b76dd3
Sebastián Quintero <sq07369@gmail.com>
2026-01-18 21:33:29 -0500

    Update Pulse.js

--- src/entities/Pulse.js
+++ src/entities/Pulse.js
@@ -3,29 +3,41 @@
 export default class Pulse {
   constructor(link) {
     this.link = link;
-    this.t = Math.random();
-    this.speed = 0.4 + Math.random() * 0.4;
+    this.progress = Math.random();
+    this.speed = 0.003 + Math.random() * 0.003;
   }
 
-  update(dt) {
-    this.t += dt * this.speed;
-    if (this.t > 1) this.t = 0;
+  update() {
+    this.progress += this.speed;
+
+    if (this.progress > 1) {
+      this.progress = 0;
+    }
   }
 
   draw(ctx, map) {
-    const a = map.latLngToContainerPoint([this.link.from.lat, this.link.from.lng]);
-    const b = map.latLngToContainerPoint([this.link.to.lat, this.link.to.lng]);
+    const from = map.latLngToContainerPoint([
+      this.link.from.lat,
+      this.link.from.lng
+    ]);
+    const to = map.latLngToContainerPoint([
+      this.link.to.lat,
+      this.link.to.lng
+    ]);
 
-    const x = a.x + (b.x - a.x) * this.t;
-    const y = a.y + (b.y - a.y) * this.t;
+    const x = from.x + (to.x - from.x) * this.progress;
+    const y = from.y + (to.y - from.y) * this.progress;
 
     ctx.save();
-    ctx.beginPath();
-    ctx.arc(x, y, 2.5, 0, Math.PI * 2);
     ctx.fillStyle = "#00ffcc";
     ctx.shadowBlur = 10;
     ctx.shadowColor = "#00ffcc";
+
+    ctx.beginPath();
+    ctx.arc(x, y, 2.2, 0, Math.PI * 2);
     ctx.fill();
+
     ctx.restore();
   }
 }
+
