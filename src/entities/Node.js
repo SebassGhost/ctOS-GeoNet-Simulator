b444b3f348950d4e0fd392d3cb2a4a42b52f1222
Sebastián Quintero <sq07369@gmail.com>
2026-01-18 20:28:31 -0500

    Update Node.js

--- src/entities/Node.js
+++ src/entities/Node.js
@@ -1,3 +1,5 @@
+// src/entities/Node.js
+
 export default class Node {
   constructor({ id, lat, lng, type = "device", status = "normal" }) {
     this.id = id;
@@ -8,10 +10,12 @@ export default class Node {
   }
 
   draw(ctx, map) {
-    const pos = map.latLngToContainerPoint([this.lat, this.lng]);
+    const point = map.latLngToContainerPoint([this.lat, this.lng]);
+
+    const radius = this.type === "ctos" ? 12 : 5;
 
     ctx.beginPath();
-    ctx.arc(pos.x, pos.y, this.type === "ctos" ? 12 : 5, 0, Math.PI * 2);
+    ctx.arc(point.x, point.y, radius, 0, Math.PI * 2);
 
     ctx.fillStyle =
       this.status === "normal" ? "#00ffcc" :
