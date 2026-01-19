3e8e998ac845df2a1bded0f106d184b1d17ec1ac
Sebastián Quintero <sq07369@gmail.com>
2026-01-18 20:38:15 -0500

    Update Link.js

--- src/entities/Link.js
+++ src/entities/Link.js
@@ -1,1 +1,23 @@
+// src/entities/Link.js
+
+export default class Link {
+  constructor(fromNode, toNode) {
+    this.from = fromNode;
+    this.to = toNode;
+  }
+
+  draw(ctx, map) {
+    const p1 = map.latLngToContainerPoint([this.from.lat, this.from.lng]);
+    const p2 = map.latLngToContainerPoint([this.to.lat, this.to.lng]);
+
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+
+    ctx.strokeStyle = "rgba(0, 255, 204, 0.35)";
+    ctx.lineWidth = 1;
+
+    ctx.stroke();
+  }
+}
 
